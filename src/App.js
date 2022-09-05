a805b0f4848e9e0f473d1329663924a7c5572afe
oper0116 <oper0116@gmail.com>
2022-09-05 23:03:21 +0900

    feat: 프로젝트 구성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import h from "react-hyperscript";
+import helpers from "hyperscript-helpers";
 
 import "./App.css";
 
+const { div, h1, h2 } = helpers(h);
+
 function App() {
 	// return h("div.App", h("header.App-header", h("img.App-log", { src: logo, alt: "logo" }), h("p", "Edit <code>src/App.js</code> and save to reload.")));
-	return h("div.example", [h("h1#heading", "This is hyperscript"), h("h2", "creating React.js markup")]);
+
+	return div(".exmaple", [h1("#heading", "This is hyperscript uses helpers"), h2("creating React.js markup")]);
 }
 
 export default App;
