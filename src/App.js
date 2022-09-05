fc4f65266572b268059a8de46f84b17122ea4688
oper0116 <oper0116@gmail.com>
2022-09-05 23:10:40 +0900

    feat: list 테스트

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import h from "react-hyperscript";
 import helpers from "hyperscript-helpers";
 
 import "./App.css";
+import React from "react";
 
-const { div, h1, h2 } = helpers(h);
+const { div, h1, h2, ul, li } = helpers(h);
 
 function App() {
 	// return h("div.App", h("header.App-header", h("img.App-log", { src: logo, alt: "logo" }), h("p", "Edit <code>src/App.js</code> and save to reload.")));
 
-	return div(".exmaple", [h1("#heading", "This is hyperscript uses helpers"), h2("creating React.js markup")]);
+	const lists = ["a", "b"];
+
+	return h(React.Fragment, [
+		[
+			div(".exmaple", [h1("#heading", "This is hyperscript uses helpers"), h2("creating React.js markup")]),
+			ul(
+				".list",
+				lists.map((item, index) => li(".list", item))
+			),
+		],
+	]);
 }
 
 export default App;
